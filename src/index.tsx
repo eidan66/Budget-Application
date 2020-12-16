c4c06e8677544a40037f8052b887b5b36dc6fc7f
Idan Levian <eidan47@gmail.com>
2020-12-16 12:50:53 +0200

    feat-1 adding material-ui packge

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { themeConfig } from './style/theme/liteTheme';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={themeConfig}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
