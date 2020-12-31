59325a627ef95fe194cbeda73955c31dac0984b4
Idan <eidan47@gmail.com>
2020-12-31 12:35:03 +0200

    feat-6 create current balance molecule component

---
+++ src/components/molecules/CurrentBalance/style.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import { palette, PaletteProps, spacing, SpacingProps } from '@material-ui/system'; //From material-ui box style.
+
+export const CurrentBalanceWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  min-height: 4rem;
+`;
+export const Box = styled.div<PaletteProps & SpacingProps>`
+  ${palette};
+  ${spacing};
+  display: inline-block;
+  padding: 1rem;
+  border-radius: 1rem;
+  background-color: #9c8fe775;
+`;
