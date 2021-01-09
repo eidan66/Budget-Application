e977849015182f1eb849fda19b34b88c4df739a4
Idan <eidan47@gmail.com>
2021-01-09 17:29:33 +0200

    feat-17 add typography title

--- src/views/pages/Categories/Categories.tsx
+++ src/views/pages/Categories/Categories.tsx
@@ -1,13 +1,12 @@
 import React, { FC } from 'react';
 import {} from '../../../components/organisms';
 import { ExpenseMiniCard } from '../../../components/molecules';
+import { Typography } from '../../../components/atoms';
 import db from '../../../mockup-tests/Payment_History.json';
 import * as S from './style';
 import { getRandomColor } from '../../../helpers';
 
-export const categoriesRender = (database, categories) => {
-  //   const categories = [];
-
+export const categoriesRender = (database, categories, categoriesColors) => {
   return database.map((data) => {
     let flag = false;
 
@@ -17,10 +16,12 @@ export const categoriesRender = (database, categories) => {
 
     if (!flag) {
       categories.push(data.category);
+      const color = getRandomColor();
+      categoriesColors.push(color);
       flag = false;
       return (
         <S.CardWrapper>
-          <ExpenseMiniCard.Category category={data.category} color={getRandomColor()} />
+          <ExpenseMiniCard.Category category={data.category} color={color} />
         </S.CardWrapper>
       );
     }
@@ -34,8 +35,17 @@ interface ICategoryProp {
 
 const Categories: FC<ICategoryProp> = () => {
   const categories = [];
+  const categoriesColors = [];
 
-  return <S.CategoryWrapper>{categoriesRender(db, categories)}</S.CategoryWrapper>;
+  return (
+    <S.CategoryWrapper>
+      <S.TitleWrapper>
+        <Typography.MediumText color="primary">Categories</Typography.MediumText>
+      </S.TitleWrapper>
+
+      <S.CategoriesWrapper>{categoriesRender(db, categories, categoriesColors)}</S.CategoriesWrapper>
+    </S.CategoryWrapper>
+  );
 };
 
 export default Categories;
--- src/views/pages/Categories/style.tsx
+++ src/views/pages/Categories/style.tsx
@@ -8,8 +8,7 @@ import styled from 'styled-components';
 
 export const CategoryWrapper = styled.div`
   display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
+  flex-direction: column;
   background-color: #f1edee;
   border-radius: 1rem;
   padding: 1rem;
@@ -17,6 +16,18 @@ export const CategoryWrapper = styled.div`
   height: 64rem;
 `;
 
+export const TitleWrapper = styled.div`
+  margin-bottom: 2rem;
+`;
+
+export const CategoriesWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
 export const CardWrapper = styled.div`
   flex: 1 0 21%;
+  margin-bottom: 1rem;
 `;
