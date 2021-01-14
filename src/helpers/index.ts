a474ed74f7f62e3d647a813c3c51a8c17f2f45e2
Idan <eidan47@gmail.com>
2021-01-14 16:08:37 +0200

    feat-21 create sort function by date **only**

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,23 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-prototype-builtins */
+
+export const compareFunction = (elmA: { date: string }, elmB: { date: string }) => {
+  const partsOfDateA = elmA.date.split('/');
+  const comparableDateA = `${partsOfDateA[2]}-${partsOfDateA[0].padStart(2, '0')}-${partsOfDateA[1].padStart(2, '0')}`;
+
+  const partsOfDateB = elmB.date.split('/');
+  const comparableDateB = `${partsOfDateB[2]}-${partsOfDateB[0].padStart(2, '0')}-${partsOfDateB[1].padStart(2, '0')}`;
+
+  if (comparableDateA > comparableDateB) {
+    return -1;
+  }
+  if (comparableDateA < comparableDateB) {
+    return 1;
+  }
+  return 0;
+};
+
 export const numberWithCommas = (number: string): string => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
 export const amountColorPicker = (type: { paymentType: string }) =>
