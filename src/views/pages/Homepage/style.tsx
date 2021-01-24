3c559b5ab51670293abe350ab2f9a480b1d9c508
Idan <eidan47@gmail.com>
2021-01-25 01:27:00 +0200

    feat-23 create homepage for defualt router page

---
+++ src/views/pages/Homepage/style.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HomepageWrapper = styled.div`
+  display: flex;
+  border-radius: 1rem;
+  box-shadow: 0px 10px 15px -3px rgba(8, 8, 8);
+  width: auto;
+`;
+
+export const NavbarWrapper = styled.div``;
+
+export const PaymentWrapper = styled.div``;
