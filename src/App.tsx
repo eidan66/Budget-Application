574ccd129a48df01a486a833ae28146c770f56a5
Idan <eidan47@gmail.com>
2021-01-25 02:15:15 +0200

    feat-24 add backdrop to loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,36 @@
 import React, { useEffect, useContext, useState } from 'react';
 import * as S from './style';
 import DATABASE from './mockup-tests/Payment_History.json';
+
+//  ************* Backdrop *************   \\
+import { Backdrop } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+//  ************* Backdrop *************   \\
+
 import USER_DETAILS from './mockup-tests/userDetails.json';
 import { compareFunction, categoriesCollector } from './helpers';
+
+//  ************* CONTEXT *************   \\
 import { UserContext } from './contexts/user/userContext';
 import { AppContext } from './contexts/app/appContext';
 import { PaymentContext } from './contexts/payment/paymentContext';
 import { CategoriesContext } from './contexts/categories/categoriesContext';
+//  ************* CONTEXT *************   \\
+
 import { Loader } from './components/atoms';
 
 import Homepage from './views/pages/Homepage/index';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+  })
+);
+
 const App: React.FC = () => {
+  const classes = useStyles();
   const [userFlag, setUserFlag] = useState(false);
   const { sorted, categoriesFlag, setSorted, setCategoriesFlag } = useContext(AppContext);
   const { paymentDetails, setPaymentDetails } = useContext(PaymentContext);
@@ -52,7 +71,9 @@ const App: React.FC = () => {
           last_name={userDetails[0].last_name}
         />
       ) : (
-        <Loader.Spinner />
+        <Backdrop className={classes.backdrop} open={true}>
+          <Loader.Spinner />
+        </Backdrop>
       )}
     </S.AppWrapper>
   );
