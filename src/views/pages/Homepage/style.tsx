adb0aca0f2818e95574716742df869641f986a19
Idan <eidan47@gmail.com>
2021-01-30 10:50:33 +0200

    refactor-5 change box-shadow

--- src/views/pages/Homepage/style.tsx
+++ src/views/pages/Homepage/style.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const HomepageWrapper = styled.div`
   display: flex;
   border-radius: 1rem;
-  box-shadow: 0px 10px 15px -3px rgba(8, 8, 8);
+  box-shadow: 0px 0px 15px 1px rgba(8, 8, 8);
   width: auto;
 `;
 
