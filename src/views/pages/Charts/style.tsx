06322d0a3620afcc56cf93d946c0732a4c1ed77a
Idan <eidan47@gmail.com>
2021-02-04 19:46:05 +0200

    style-1 move inline style to style file.

---
+++ src/views/pages/Charts/style.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ChartsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 86rem;
+`;
+
+export const PaymentChartWrapper = styled.div`
+  box-shadow: 0 0 5px #333;
+  border-radius: 1rem;
+  padding: 1rem;
+  width: 70rem;
+  margin: 0 auto;
+`;
