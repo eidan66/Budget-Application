06d775059f5fdff6a727ac7edef9f60585948c43
Idan <eidan47@gmail.com>
2021-02-11 15:19:39 +0200

    style-6 fix map height jump

--- src/components/organisms/Map/style.tsx
+++ src/components/organisms/Map/style.tsx
@@ -4,6 +4,7 @@ export const MapWrapper = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
+  overflow-y: hidden;
 `;
 export const ExitWrapper = styled.div`
   display: flex;
